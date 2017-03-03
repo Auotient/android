57ea2e5441d93a46a980772d1a4b63de7f34ba8b
Auotient <844893980@qq.com>
2017-03-03 10:56:25 +0800

    2017年3月3日10:56:15 ---add by  wzy

--- FaceRecognitionDemo/src/com/example/facerecognitiondemo/MainActivity.java
+++ FaceRecognitionDemo/src/com/example/facerecognitiondemo/MainActivity.java
@@ -1,25 +1,49 @@
 package com.example.facerecognitiondemo;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import com.iflytek.cloud.Setting;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * 人脸识别示例
+ */
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-	
+		
+		
+		findViewById(R.id.btn_online_demo).setOnClickListener(MainActivity.this);
+		findViewById(R.id.btn_offline_demo).setOnClickListener(MainActivity.this);
+		findViewById(R.id.btn_video_demo).setOnClickListener(MainActivity.this);
+		
+		Setting.setShowLog(true);
 	}
 
-
+	@Override
+	public void onClick(View v) {
+		Intent intent = null;
+		switch (v.getId()) {
+		case R.id.btn_online_demo:
+			intent = new Intent(MainActivity.this, OnlineFaceDemo.class);
+			startActivity(intent);
+			break;
+		case R.id.btn_offline_demo:
+			intent = new Intent(MainActivity.this, OfflineFaceDemo.class);
+			startActivity(intent);
+			break;
+		case R.id.btn_video_demo:
+			intent = new Intent(MainActivity.this, VideoDemo.class);
+			startActivity(intent);
+			break;
+		default:
+			break;
+		}
+	}
 }
